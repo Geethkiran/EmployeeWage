0f8d8f510a006ce4d08e2b5483823aca9d48b7b3
Geethkiran <geeth53xy@gmail.com>
2020-03-07 13:28:43 +0530

    [Geeth]Refactor. added employee present or absent

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "****** Welcom to Employee Wage ***"
+
+IS_PRESENT=1
+
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq $IS_PRESENT ]
+then
+	echo "Employee Present"
+else
+	echo "Employee Absent"
+fi
