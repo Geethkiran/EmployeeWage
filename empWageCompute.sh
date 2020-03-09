28dd2ef63ffb9bb1ebac3403c8eb4535a1198278
Geethkiran <geeth53xy@gmail.com>
2020-03-09 09:50:43 +0530

    [geeth]Refactor. wage per day is computed

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -3,11 +3,13 @@
 echo "****** Welcom to Employee Wage ***"
 
 IS_PRESENT=1
-
+WAGE_PERHOUR=20
+WORKING_HOURS_PERDAY=8
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq $IS_PRESENT ]
 then
-	echo "Employee Present"
+	total_wage_perday=$(($WAGE_PERHOUR*$WORKING_HOURS_PERDAY))
+	echo "$total_wage_perday"
 else
 	echo "Employee Absent"
 fi
