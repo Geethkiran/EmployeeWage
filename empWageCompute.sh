0c55e7f9b85c584c44e3ce60b4a9c16d610af368
Geethkiran <geeth53xy@gmail.com>
2020-03-11 11:11:51 +0530

    [geeth]Refactor. days and daily wages are stored in dictionary

--- empWageCompute.sh
+++ empWageCompute.sh
@@ -6,12 +6,11 @@ MAX_WORKING_HOURS=100
 total_hours=0
 Workingdays=0
 total_wage=0
-IS_PRESENT=1
 IS_FULLTIME=1
 IS_PARTTIME=2
 IS_ABSENT=0
 WAGE_PERHOUR=20
-count=0
+declare -A wagesPerday
 getemphr(){
 	hourCheck=$1
 case $hourCheck in
@@ -32,10 +31,12 @@ do
 	workinghrs="$( getemphr $(($RANDOM%3)))"
 	total_hours=$(($workinghrs+$total_hours))
 	total_wage_perday=$(($WAGE_PERHOUR*$workinghrs))
-	wagesPerday[count]=$total_wage_perday
+	day="Day"
+	wagesPerday[" $day $Workingdays"]="$total_wage_perday"
 	((count++))
 	total_wage=$(($total_wage_perday+$total_wage))
 	((Workingdays++))
 done
+echo "${!wagesPerday[@]}"
 echo "daily wages: ${wagesPerday[@]}"
 echo "total wage: $total_wage"
